f1e42331795561dd3a956cf8ce123b3debaf56ca
Miguel Jerónimo <mjeronimo@frd.org.mx>
2025-01-03 19:06:39 -0600

    ReadJson

---
+++ app/src/main/java/com/miguel/tibiamap/utils/JsonInfo.kt
@@ -0,0 +1,33 @@
+package com.miguel.tibiamap.utils
+
+import android.content.Context
+import android.util.Log
+import com.google.gson.Gson
+import com.miguel.tibiamap.domain.Coordinates
+import org.apache.commons.io.IOUtils
+import java.io.IOException
+
+class JsonInfo {
+
+    fun parseWithGson(context: Context) {
+        val jsonString = loadJSONFromAsset(context, "data.json")
+        val gson = Gson()
+        val user = gson.fromJson(jsonString, Coordinates::class.java)
+        //Log.d("JSON", "Name: ${user.name}, Age: ${user.age}")
+    }
+
+    fun loadJSONFromAsset(context: Context, fileName: String): String? {
+        return try {
+            val inputStream = context.assets.open(fileName)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            String(buffer, Charsets.UTF_8)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            null
+        }
+    }
+
+}
